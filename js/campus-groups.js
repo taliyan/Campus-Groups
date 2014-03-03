c3d340cd86233b2b8e5fb6c6e90df7258d814e7c
MIkal Morello <mikalmorello@gmail.com>
2014-03-03 17:51:49 -0500

    Template Update
    Mocking up template in accordance with concept design

---
+++ js/campus-groups.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+
+
+// LEFT NAVIGATION SLIDE OUT
+$(document).ready(function () {
+  $('[data-toggle=offcanvas]').click(function () {
+    $('.row-offcanvas').toggleClass('active')
+  });
+});
